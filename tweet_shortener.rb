709542565c8092c2ef57e9a8838d058e80a9ffa3
Tracy Ngo <tngo1206@gmail.com>
2019-02-22 19:11:33 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+require 'pry'
+
+def dictionary
+  replacement_words = {:'hello' => 'hi', :'to' => '2', :'two' => '2', :'too' => '2', :'for' => '4', :'four' => '4', :'be' => 'b', :'you' => 'u', :'at' => '@', :'and' => '&'}
+end
+  
+def word_substituter(tweet)
+  # replaces long words with their expected short form
+    tweet.split(" ").map do |word| #creates the array of tweet words and iterates over each word
+      if dictionary.keys.include?(word.downcase) #on the iteration, checks if dictionary includes tweet word  which is also downcased
+        word = dictionary[word.downcase] #if the condition is met the tweet word local variable gets assigned to the dictionary key
+      else
+        word #if condition is not met return the word to the array
+    end
+  end.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+   puts word_substituter(tweet)
+  end
+end 
+
+
+  
