ffb274f262f1de6dcaa875569ee53bfb8d242ef4
Tracy Ngo <tngo1206@gmail.com>
2019-02-22 19:23:42 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,7 +2,7 @@
 require 'pry'
 
 def dictionary
-  replacement_words = {'hello' => 'hi', 'to' => '2', 'two' => '2', 'too' => '2', :'for' => '4', :'four' => '4', :'be' => 'b', :'you' => 'u', :'at' => '@', :'and' => '&'}
+  replacement_words = {'hello' => 'hi', 'to' => '2', 'two' => '2', 'too' => '2', 'for' => '4', 'four' => '4', 'be' => 'b', 'you' => 'u', 'at' => '@', 'and' => '&'}
 end
   
 def word_substituter(tweet)
